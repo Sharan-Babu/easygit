cd18ddcb623b7996e84d24150cf45908adc15925
Sharan Babu <50396375+Sharan-Babu@users.noreply.github.com>
2021-07-13 19:08:23 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -2,4 +2,4 @@ from easygit.easygit import Easygit
 
 def test_app():
     git = Easygit()
-    assert git.query('h') == "Try another query"
+    assert git.query('h') == "Try another query or visit https://git-scm.com/docs"
